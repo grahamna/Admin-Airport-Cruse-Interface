86b250c7b18e42e5e1b75d1f03f9a4ce51c0625a
grahamna <nate.a.graham@gmail.com>
2019-03-05 21:58:07 -0800

    Airports don't have airlines, Airports are simply Destinations/Sources

--- src/concreteObjects/Airport.java
+++ src/concreteObjects/Airport.java
@@ -1,12 +1,10 @@
 package concreteObjects;
 
-import java.util.HashSet;
 
 /*Airport: Objects of this class represent airports.  The only information maintained is the name, which must be exactly 3 characters in length. */
 
 public class Airport {
     private String name; //char len 3, must be unique
-    private HashSet<Airline> airlineList = new HashSet<Airline>();
 
     public Airport(String name){
         assert name.length()==3:"Airport name.len must be 3";
@@ -17,7 +15,7 @@ public class Airport {
 
     @Override
     public String toString(){
-        String str = "Airport "+this.name;
+        String str = "Airport: "+this.name;
         return str;
     }
 }
