3b45f51042297d9a9bcf0da5beca7316747d8f83
grahamna <nate.a.graham@gmail.com>
2019-03-06 16:06:45 -0800

    systemmanager

--- src/SystemManager/SystemManager.java
+++ src/SystemManager/SystemManager.java
@@ -15,8 +15,8 @@ public class SystemManager {
         }
         else {
             Airport airport = new Airport(n);
-            if(searchAirports(n)!=null) {
-               System.out.println("Airport "+n+" already exists.");
+            if(airports.contains(airport)){
+                System.out.println("Airport "+n+" already exists.");
             }
             else{
                 airports.add(airport);
@@ -31,7 +31,7 @@ public class SystemManager {
         }
         else {
             Airline airline = new Airline(n);
-            if(searchAirlines(n)!=null) {
+            if(airlines.contains(airline)) {
                 System.out.println("Airline "+n+" already exists.");
             }
             else{
@@ -50,30 +50,26 @@ public class SystemManager {
         return null;
     }
 
-    Airport searchAirports(String n) {
-        for(Airport ap: airports) {
-            if(ap.getName().equals(n)) {
-                return ap;
-            }
-        }
-        return null;
-    }
-
     public void createFlight(String aname, String orig, String dest, int year, int month, int day, String id) {
-        Airline al = searchAirlines(aname);
-        if(orig.equals(dest)) {
-            System.out.println("Originating airport ("+orig+") cannot be the same as the destination airport.");
-        }
-        else {
-            Airport origin = searchAirports(orig);
-            Airport destination = searchAirports(dest);
-            if(origin != null && destination != null) {
-                Flight flight = new Flight(origin, destination, id);
-                System.out.println("Created flight "+id+" from "+orig+" to "+dest+".");
+        Airline al = new Airline(aname);
+        if (airlines.contains(al)){
+            if(orig.equals(dest)) {
+                System.out.println("Originating airport ("+orig+") cannot be the same as the destination airport.");
             }
             else {
-                System.out.println("Airports "+orig+" and/or "+dest+" don't exist.");
+                Airport origin = new Airport(orig);
+                Airport destination = new Airport(dest);
+                if(airports.contains(origin) && airports.contains(destination)) {
+                    Flight flight = new Flight(origin, destination, id);
+                    System.out.println("Created flight "+id+" from "+orig+" to "+dest+".");
+                }
+                else {
+                    System.out.println("Airports "+orig+" and/or "+dest+" don't exist.");
+                }
             }
+        }
+        else {
+            System.out.println("Airline "+al.toString()+" missing");
         }
     }
 
