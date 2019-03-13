6d649c02b20831f930391fa678532846cf7d5f61
grahamna <nate.a.graham@gmail.com>
2019-03-13 15:12:03 -0700

    .

--- src/local/Date.java
+++ src/local/Date.java
@@ -1,16 +1,18 @@
 package local;
 
 public class Date{
-    private int day, year, month;
+    private int day, year, month, hour, min;
 
-    public Date(int m, int d, int y) {
-        day=d;
-        year=y;
-        month=m;
+    public Date(int m, int d, int y, int h, int min) {
+        this.day=d;
+        this.year=y;
+        this.month=m;
+        this.hour=h;
+        this.min=min;
     }
 
     @Override
     public String toString() {
-        return month+"/"+day+"/"+year;
+        return month+"/"+day+"/"+year+" at "+hour+":"+min;
     }
 }
